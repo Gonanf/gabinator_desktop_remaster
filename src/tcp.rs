ee5702e0fca5d393cd4d64c24ec8d0d3cf8fca4e
Gabriel <gabysolotorevsky100@gmail.com>
2024-11-17 19:43:52 -0300

    fixed tcp

--- src/tcp.rs
+++ src/tcp.rs
@@ -1,8 +1,16 @@
+use image::{ImageBuffer, RgbaImage};
+use std::collections::HashMap;
+use std::fs::File;
+use turbojpeg::Image;
+
+use crate::Logger;
+use crate::{capture::capture_screen, error::GabinatorError};
+use std::io::Read;
+use std::net::TcpStream;
 use std::{io::Write, net::TcpListener};
 
-use crate::capture::capture_screen;
-
-pub fn start_server() {
+pub fn start_server( test_server: bool, test_data: bool,quality: u8) {
+    let config = Logger::get_config_content();
     let socket = TcpListener::bind("0.0.0.0:3000").unwrap();
     let ip = local_ip_address::local_ip().unwrap();
     println!("SERVER IP -> {:?}", ip);
@@ -10,17 +18,84 @@ pub fn start_server() {
         println!("Conectado");
         let mut client = st.unwrap();
         let mut tries = 0;
-        loop {
-            if client.write_all(&capture_screen().unwrap()).is_err() {
-                tries += 1;
-                println!("Package failed, {} more tries remaining", 5 - tries);
-                if tries >= 5 {
-                    println!("Connection with too many errors");
-                    break;
-                }
-            } else {
-                tries = 0;
+        if test_server{
+            if test_data{
+                if client.write_all(&[10,8,8,8,8,8,8,b'\n']).is_err() {
+                    GabinatorError::newCapture(
+                        format!("Error sending test data"),
+                        crate::error::LoggerLevel::Error,
+                        Some(config.clone()));
+                    }
             }
+            else{send_image_data(config.clone(),&mut client,quality);}
+        }
+        else{
+            loop {
+                if test_data{
+                    if client.write_all(&[10,8,8,8,8,8,8,b'\n']).is_err() {
+                        GabinatorError::newCapture(
+                            format!("Error sending test data"),
+                            crate::error::LoggerLevel::Error,
+                            Some(config.clone()));
+                        }
+                }
+                else{
+                    let value = send_image_data(config.clone(),&mut client,quality);
+                if value.is_some() {
+                    tries += 1;
+                    println!("Package failed, {} more tries remaining", 5 - tries);
+                    if tries >= 5 {
+                        println!("Connection with too many errors");
+                        break;
+                    }
+                } else {
+                    tries = 0;
+                }
+                }
+            }
+        }
+        
+    }
+}
+
+fn send_image_data(config: HashMap<String,String>,  client: &mut TcpStream, quality: u8) -> Option<GabinatorError> {
+    let mut data = capture_screen(quality).unwrap();
+    let mut pdata = Vec::new();
+    pdata.push(u8::try_from(usize::BITS).unwrap());
+    pdata.append(&mut  data.len().to_be_bytes().to_vec());
+    pdata.append(&mut data);
+
+    if client.write_all(&pdata).is_err() {
+        return Some(GabinatorError::newCapture(format!("Error writing data into client"), crate::error::LoggerLevel::Error, Some(config.clone())))
+    }
+    /*if client.flush().is_err() {
+        return Some(GabinatorError::newCapture(
+            format!("Error flushing server"),
+            crate::error::LoggerLevel::Error,
+            Some(config.clone()),
+        ));
+    } */
+    None
+}
+
+pub fn test_server() {
+    let config = Logger::get_config_content();
+    let mut server = TcpStream::connect("0.0.0.0:3000").unwrap();
+    let mut total_buffer = File::create("amongas.jpg").unwrap();
+    let mut total_bytes = 0;
+    let mut iteration = 0;
+    loop {
+        let mut buffer = [0; 1024];
+        let bytes = server.read(&mut buffer).unwrap();
+        total_buffer.write_all(&buffer);
+        total_bytes += bytes;
+        dbg!(&bytes);
+        if bytes < 1024 {
+            let mut test = String::new();
+            dbg!(&total_bytes);
+            total_bytes = 0;
+            iteration += 1;
+            total_buffer = File::create(format!("amongas{iteration}.jpg")).unwrap();
         }
     }
 }
